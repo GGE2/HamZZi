74e04742c6237386c0e3f0576cd62f2d19e766e7
kimjihun <jihoon_story@naver.com>
2023-01-16 13:12:35 +0900

    Init: 프로젝트 초기화

---
+++ mobile/app/src/main/java/com/team/teamrestructuring/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.team.teamrestructuring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
