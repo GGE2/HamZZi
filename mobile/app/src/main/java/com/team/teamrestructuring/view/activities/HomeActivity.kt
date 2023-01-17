b035117945caedea1da5660302efa2bf1dee91da
kimjihun <jihoon_story@naver.com>
2023-01-17 14:44:44 +0900

    Init: 프로젝트 설정 파일 추가

---
+++ mobile/app/src/main/java/com/team/teamrestructuring/view/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.team.teamrestructuring.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team.teamrestructuring.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- mobile/app/src/main/java/com/team/teamrestructuring/view/activities/LoginActivity.kt
+++ mobile/app/src/main/java/com/team/teamrestructuring/view/activities/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.team.teamrestructuring
+package com.team.teamrestructuring.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team.teamrestructuring.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
