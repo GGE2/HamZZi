d7d1a12af6d05850e575cf4c7bcd3d83c1323a7f
jesniey11@gmail.com <jesniey11@gmail.com>
2023-01-18 17:30:47 +0900

    Feat: Firebase 연결 및 회원가입(3)

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "유저 API", tags = {"User"})
 @RestController
 //임시 주소...
 @RequestMapping("api/user")
@@ -18,6 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping()
+    @ApiOperation(value = "회원 가입", notes = "필요한 정보를 전부 입력한다")
     public ResponseEntity registerUser(@RequestBody UserLogin user) {
         return userService.registerUser(user);
     }
