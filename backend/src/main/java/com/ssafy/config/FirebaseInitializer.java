5e05ae339adb308adeecfceaaf73699889fc31f5
jesniey11@gmail.com <jesniey11@gmail.com>
2023-01-19 15:46:19 +0900

    Feat: 회원가입 완료

--- backend/src/main/java/com/ssafy/config/FirebaseInitializer.java
+++ backend/src/main/java/com/ssafy/config/FirebaseInitializer.java
@@ -3,6 +3,7 @@ package com.ssafy.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +23,11 @@ public class FirebaseInitializer {
 
         FirebaseApp app = FirebaseApp.initializeApp(options);
         return app;
+    }
+
+    @Bean
+    public FirebaseAuth getFirebaseAuth() throws IOException {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp());
+        return firebaseAuth;
     }
 }
--- backend/src/main/java/com/ssafy/db/entity/User/UserProfile.java
+++ backend/src/main/java/com/ssafy/db/entity/User/UserProfile.java
@@ -1,4 +1,4 @@
-package com.ssafy.db.entity;
+package com.ssafy.db.entity.User;
 
 public class UserProfile {
     //Create for git commit
