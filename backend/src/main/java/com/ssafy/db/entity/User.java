5fd87e2277b49f6d796cc1d12a3199ece1bb6af0
jesniey11@gmail.com <jesniey11@gmail.com>
2023-01-19 14:27:50 +0900

    Feat: 회원가입 완료

--- backend/src/main/java/com/ssafy/db/entity/User.java
+++ backend/src/main/java/com/ssafy/db/entity/User.java
@@ -1,10 +1,11 @@
 package com.ssafy.db.entity;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
-    private int user_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
 
 }
