9729c3e380f8e5575e1507151c09edf805e45180
고청천 <g2c1010@naver.com>
2023-01-20 09:07:53 +0900

    backup login

--- mobile/app/src/main/java/com/team/teamrestructuring/view/activities/LoginActivity.kt
+++ mobile/app/src/main/java/com/team/teamrestructuring/view/activities/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.team.teamrestructuring.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.team.teamrestructuring.R
+import com.team.teamrestructuring.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var te:TextView = binding.googleSignIn.getChildAt(0) as TextView
+        te.text = "구글 계정으로 로그인"
     }
+    // 카카오 로그인
+
 }
