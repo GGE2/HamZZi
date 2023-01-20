b034951f23b2f3ae86529174272d96aa40e6acaf
jkjk0903 <jkjk0903@gmail.com>
2023-01-20 15:53:59 +0900

    Feat: 회원가입 CORS 이슈 해결 진행중

---
+++ proxy_fix/src/KakaoLoginData.js
@@ -0,0 +1,3 @@
+export const REST_API_KEY = "f8b2efed7065bfbb447dc9d993054da2";
+export const REDIRECT_URI = "http://localhost:3000";
+// export const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
