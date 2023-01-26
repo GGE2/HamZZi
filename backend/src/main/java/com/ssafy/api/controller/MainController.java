d8fec68db59cf3a72f8dca26cfd8aef5f42bfe54
jesniey11@gmail.com <jesniey11@gmail.com>
2023-01-26 17:58:34 +0900

    Feat: Kakao 토큰 추가(2)

---
+++ backend/src/main/java/com/ssafy/api/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.ssafy.api.controller;
+
+import com.ssafy.api.service.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @Autowired
+    KakaoService kakaoService;
+
+    @RequestMapping("api/kakao")
+    @ResponseBody
+    public String testConnect() {
+        return "연결성공";
+    }
+    @RequestMapping("kakao/sign_in")
+    public String kakaoSignIn(@RequestParam("code") String code) {
+        Map<String,Object> result = kakaoService.execKakaoLogin(code);
+        return "redirect:webauthcallback://success?customToken="+result.get("customToken").toString();
+    }
+}
