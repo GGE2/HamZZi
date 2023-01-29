4f32c7231b7d63fec769b2d1021965df2b825da9
jesniey11 <jesniey11@gmail.com>
2023-01-30 01:34:38 +0900

    Fix: User CRUD Controller 수정 #299 #300 #301 (마무리 필요)

--- backend/src/main/java/com/ssafy/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/api/service/UserService.java
@@ -11,6 +11,5 @@ public interface UserService {
     Long registerUser(UserRegisterRequest registerInfo);
     void registerNickname(UserNicknameRequest nicknameInfo);
     UserProfile loginUserData(String email);
-//    Long updateUser(Long user_id, UserUpdateRequest updateInfo);
     void deleteUser(String email);
 }
