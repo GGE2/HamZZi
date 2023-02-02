d71fbd50837b97bd096f25692dba0e251672d352
jkjk0903 <jkjk0903@gmail.com>
2023-02-02 21:38:56 +0900

    Fix: merge 누락 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Main from "./pages/Main";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 import KakaoLogin from "./pages/loginpages/KakaoLogin";
+import StatCtrl from "./pages/mainpages/hampages/statuspages/StatCtrl";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Route index element={<Login />} />
       <Route path="/kakaoLogin" element={<KakaoLogin />} />
 
-        <Route path="/main" element={<Main />} />
-
+      <Route path="/main" element={<Main />} />
+      <Route path="/statctrl" element={<StatCtrl />} />
     </Routes>
   );
 }
