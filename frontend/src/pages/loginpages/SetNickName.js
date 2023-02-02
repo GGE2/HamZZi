090e50457d2c0d004c7ea443bf96c03f9b911835
jkjk0903 <jkjk0903@gmail.com>
2023-02-02 13:14:20 +0900

    Feat: 사용자 닉네임 등록

--- frontend/src/pages/loginpages/SetNickName.js
+++ frontend/src/pages/loginpages/SetNickName.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import axios from "axios";
+import "../../styles/Modal.css";
 
 const SetNickName = () => {
   const [nickName, SetNickName] = useState("");
@@ -13,8 +14,9 @@ const SetNickName = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
+      // 요청 주소 수정 예정
       .post("http://3.35.88.23:8080/api/user/nickname", {
-        nickname: nickName,
+        user_nickname: nickName,
       })
       .then((res) => {
         console.log(res);
@@ -22,17 +24,25 @@ const SetNickName = () => {
       })
       .catch((err) => {
         console.log(err);
+        SetNickName("");
         alert("사용할 수 없는 이름입니다!");
       });
   };
   return (
-    <div className="modal">
-      <h1>환영합니다!</h1>
-      <h2>닉네임을 설정해 주세요</h2>
-      <form onSubmit={handleSubmit}>
-        <input name="nickname" value="nickName" onChange={handleChange} />
-        <button type="submit"> 설정하기</button>
-      </form>
+    <div className="Modal">
+      <div className="modalBody">
+        <h1>환영합니다!</h1>
+        <h2>닉네임을 설정해 주세요</h2>
+        <p>설정하신 닉네임은 다시 바꿀 수 없습니다</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="nickname"
+            placeholder="닉네임입력"
+            onChange={handleChange}
+          />
+          <button type="submit"> 설정하기</button>
+        </form>
+      </div>
     </div>
   );
 };
