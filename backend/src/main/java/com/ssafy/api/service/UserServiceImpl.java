24921966c09f143b37d5d2da37c5ea96860956af
gyocheol <kys04171@naver.com>
2023-02-16 11:58:55 +0900

    Feat: 서비스 포인트 40, 배포파일 작성

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -40,7 +40,7 @@ public class UserServiceImpl implements UserService {
 
         userProfile.setNickname(nickname);
         userProfile.setRest_point(3);
-        userProfile.setPoint(0);
+        userProfile.setPoint(40);
         user.setUserProfile(userProfile);
 
         userRepo.saveUserProfile(userProfile);
