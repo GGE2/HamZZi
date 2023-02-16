e3a97f66f252c07d252d33962c68a806cfdb545d
jkjk0903 <jkjk0903@gmail.com>
2023-02-17 02:15:26 +0900

    Fix: History 기능 수정

--- frontend/src/pages/mainpages/boardpages/History.js
+++ frontend/src/pages/mainpages/boardpages/History.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 // import History from './History';
 import HistoryItem from "./../../../components/HistoryItem";
 import { motion } from "framer-motion";
-import LoadingModal from './../../../components/LoadingModal';
+import LoadingModal from "./../../../components/LoadingModal";
 
 const History = () => {
   const nickname = localStorage.getItem("nickname");
@@ -12,7 +12,7 @@ const History = () => {
 
   const onGetTrophy = () => {
     api.get(`/api/pet/trophy/${nickname}`).then((res) => {
-      console.log(res);
+      // console.log(res);
       setTrophy(res.data);
     });
   };
@@ -36,39 +36,48 @@ const History = () => {
 
   return (
     <>
-     {loading ? (
+      <div className="DressRoom">
+        <div className="postit5"></div>
+        {loading ? (
           <>
             {" "}
             <LoadingModal />
             {/* <div className="postit"></div> */}
           </>
-        ) :<motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ y: -100, opacity: 0 }}
-        transition={{ duration: 1 }}
-        variants={variants}
-      >
-        <div className="postit5"></div>
-        <div className="HistoryHeader">
-          <div> History</div>
-        </div>
-        <div className="History">
-          {trophys.length === 0 ? <div className="historyflex">
-      <div className="HistoryItem"><img src={`shadowA.png`} alt="" /> </div></div>: null}
-      {trophys.map((trophy) => (
-            <HistoryItem
-              key={trophy.petInfo_id}
-              type={trophy.type}
-              pet_name={trophy.pet.pet_name}
-              graduate_date={trophy.pet.graduate_date}
-              length={trophys.length}
-            />
-          ))}
-        </div>
-      </motion.div>}
+        ) : (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ y: -100, opacity: 0 }}
+            transition={{ duration: 1 }}
+            variants={variants}
+          >
+            <div className="postit5"></div>
+            <div className="HistoryHeader">
+              <div> History</div>
+            </div>
+            <div className="History">
+              {trophys.length === 0 ? (
+                <div className="historyflex">
+                  <div className="HistoryItem">
+                    <img src={`shadowA.png`} alt="" />{" "}
+                  </div>
+                </div>
+              ) : null}
+              {trophys.map((trophy) => (
+                <HistoryItem
+                  key={trophy.petInfo_id}
+                  type={trophy.type}
+                  pet_name={trophy.pet.pet_name}
+                  graduate_date={trophy.pet.graduate_date}
+                  length={trophys.length}
+                />
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </div>
     </>
-    
   );
 };
 
